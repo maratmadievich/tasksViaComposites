971f2f88a36c5b3ed06f526625c2eb22b10e488d
Marat Nurgaliev <iknow.whoiam@mail.ru>
2019-10-14 20:43:29 +0300

    init

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Марат Нургалиев on 14/10/2019.
+//  Copyright © 2019 Марат Нургалиев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+

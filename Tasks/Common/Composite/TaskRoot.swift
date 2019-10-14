694b5676540d8e5930a817b70a0d5179a565578b
Marat Nurgaliev <iknow.whoiam@mail.ru>
2019-10-14 22:29:27 +0300

    commit unimportant data

---
+++ Tasks/Common/Composite/TaskRoot.swift
@@ -0,0 +1,9 @@
+//
+//  TaskRoot.swift
+//  Tasks
+//
+//  Created by Марат Нургалиев on 14/10/2019.
+//  Copyright © 2019 Марат Нургалиев. All rights reserved.
+//
+
+import Foundation
---
+++ Tasks/Modules/Tasks/Cell/TaskCellPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  TaskCellPresenter.swift
+//  Tasks
+//
+//  Created by Марат Нургалиев on 14/10/2019.
+//  Copyright © 2019 Марат Нургалиев. All rights reserved.
+//
+
+import Foundation
